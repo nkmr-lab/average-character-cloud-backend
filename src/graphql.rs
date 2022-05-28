9b4aa106b54e90369c4f57fcbe54667b7e575b31
kgtkr <contact@kgtkr.net>
2022-05-29 01:54:23 +0900

    let-elseの導入、エラー処理改善など

--- src/graphql.rs
+++ src/graphql.rs
@@ -1,19 +1,86 @@
 use std::str::FromStr;
 
 use chrono::{DateTime, Utc};
+use juniper::FieldError;
 use juniper::{
-    graphql_interface, EmptySubscription, GraphQLInputObject, GraphQLObject, RootNode, ID,
+    graphql_interface, EmptySubscription, GraphQLInputObject, GraphQLObject, IntoFieldError,
+    RootNode, ScalarValue, ID,
 };
-use juniper::{FieldError, FieldResult};
 use sqlx::PgPool;
 use ulid::Ulid;
 
 use crate::entities;
 
+#[derive(Debug)]
 pub struct AppCtx {
     pub pool: PgPool,
     pub user_id: Option<String>,
     pub now: DateTime<Utc>,
+}
+
+#[derive(Debug)]
+pub enum AppError {
+    Other(String),
+    Internal(Box<dyn std::error::Error + Send + Sync>),
+}
+
+impl<S: ScalarValue> IntoFieldError<S> for AppError {
+    fn into_field_error(self) -> FieldError<S> {
+        match self {
+            AppError::Other(msg) => msg.into(),
+            AppError::Internal(err) => {
+                log::error!("{}", err);
+                "Internal error".into()
+            }
+        }
+    }
+}
+
+type AppResult<T> = Result<T, AppError>;
+
+enum Limit {
+    First(i32),
+    Last(i32),
+}
+
+impl Limit {
+    fn encode(first: Option<i32>, last: Option<i32>) -> AppResult<Self> {
+        let max = 100;
+
+        match (first, last) {
+            (Some(first), None) => {
+                if first < 0 {
+                    Err(AppError::Other(
+                        "first must be greater than or equal to 0".to_string(),
+                    ))
+                } else if first > max {
+                    Err(AppError::Other(format!(
+                        "first must be less than or equal to {}",
+                        max
+                    )))
+                } else {
+                    Ok(Limit::First(first))
+                }
+            }
+            (None, Some(last)) => {
+                if last < 0 {
+                    Err(AppError::Other(
+                        "last must be greater than or equal to 0".to_string(),
+                    ))
+                } else if last > max {
+                    Err(AppError::Other(format!(
+                        "last must be less than or equal to {}",
+                        max
+                    )))
+                } else {
+                    Ok(Limit::Last(last))
+                }
+            }
+            _ => Err(AppError::Other(
+                "Must provide either first or last, not both".to_string(),
+            )),
+        }
+    }
 }
 
 impl juniper::Context for AppCtx {}
@@ -34,12 +101,12 @@ impl NodeID {
         let buf = base64::decode(id).ok()?;
         let s = String::from_utf8(buf).ok()?;
 
-        if let Some(record_id) = s.strip_prefix("record:") {
-            let ulid = Ulid::from_str(record_id).ok()?;
-            Some(NodeID::Record(ulid))
-        } else {
-            None
-        }
+        let Some(record_id) = s.strip_prefix("record:") else {
+            return None;
+        };
+
+        let ulid = Ulid::from_str(record_id).ok()?;
+        Some(NodeID::Record(ulid))
     }
 }
 
@@ -114,17 +181,18 @@ pub struct QueryRoot;
 
 #[juniper::graphql_object(Context = AppCtx)]
 impl QueryRoot {
-    async fn node(id: ID) -> FieldResult<Option<NodeValue>> {
+    async fn node(id: ID) -> AppResult<Option<NodeValue>> {
         Ok(None)
     }
 
     async fn records(
+        ctx: &AppCtx,
         character: Option<String>,
         first: Option<i32>,
         after: Option<String>,
         last: Option<i32>,
         before: Option<String>,
-    ) -> FieldResult<RecordConnection> {
+    ) -> AppResult<RecordConnection> {
         Ok(RecordConnection {
             page_info: PageInfo {
                 has_next_page: false,
@@ -141,24 +209,19 @@ pub struct MutationRoot;
 
 #[juniper::graphql_object(Context = AppCtx)]
 impl MutationRoot {
-    async fn create_record(ctx: &AppCtx, new_record: NewRecord) -> FieldResult<Record> {
-        let user_id = if let Some(user_id) = &ctx.user_id {
-            user_id.clone()
-        } else {
-            return Err("Authentication required".into());
+    async fn create_record(ctx: &AppCtx, new_record: NewRecord) -> AppResult<Record> {
+        let Some(user_id) = ctx.user_id.clone() else {
+            return Err(AppError::Other("Authentication required".to_string()));
+        } ;
+
+        let &[character] = new_record.character.chars().collect::<Vec<_>>().as_slice() else {
+            return Err(AppError::Other(
+                "character must be one character".to_string(),
+            ));
         };
 
-        let character =
-            if let &[character] = new_record.character.chars().collect::<Vec<_>>().as_slice() {
-                character
-            } else {
-                return Err("character must be one character".into());
-            };
-
-        let figure = if let Some(figure) = entities::figure::Figure::from_json(&new_record.figure) {
-            figure
-        } else {
-            return Err("figure must be valid json".into());
+        let Some(figure) = entities::figure::Figure::from_json(&new_record.figure) else {
+            return Err(AppError::Other("figure must be valid json".to_string()));
         };
 
         let record = entities::record::Record {
@@ -170,10 +233,10 @@ impl MutationRoot {
         };
 
         sqlx::query!(
-            r#"
-                INSERT INTO records (id, user_id, character, figure, created_at)
-                VALUES ($1, $2, $3, $4, $5)
-            "#,
+            "\
+            INSERT INTO records (id, user_id, character, figure, created_at) \
+            VALUES ($1, $2, $3, $4, $5)\
+            ",
             record.id.to_string(),
             record.user_id,
             record.character.to_string(),
@@ -181,7 +244,8 @@ impl MutationRoot {
             record.created_at,
         )
         .execute(&ctx.pool)
-        .await?;
+        .await
+        .map_err(|err| AppError::Internal(Box::new(err)))?;
 
         Ok(Record::from_entity(&record))
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(let_else, try_blocks)]
+
 pub mod app_config;
 pub mod entities;
 pub mod graphql;

99f2b49264a7259f0bb21890720b771805d21c64
kgtkr <contact@kgtkr.net>
2022-09-19 23:34:12 +0900

    update user configのversionが0の時の分岐追加

--- src/commands/user_configs_command.rs
+++ src/commands/user_configs_command.rs
@@ -23,34 +23,57 @@ pub async fn update(
         user_config.allow_sharing_figure_records = allow_sharing_figure_records;
     }
 
-    let result = sqlx::query!(
-        r#"
-            UPDATE user_configs
-                SET
-                    updated_at = $1,
-                    allow_sharing_character_configs = $2,
-                    allow_sharing_figure_records = $3,
-                    version = $4
-                WHERE
-                    user_id = $5
-                    AND
-                    version = $6
-            "#,
-        &user_config
-            .updated_at
-            .ok_or(anyhow!("updated_at is None"))?,
-        user_config.allow_sharing_character_configs,
-        user_config.allow_sharing_figure_records,
-        i32::try_from(user_config.version)?,
-        &user_config.user_id,
-        i32::try_from(prev_version)?,
-    )
-    .execute(&mut trx)
-    .await
-    .context("update character_config")?;
+    if prev_version == 0 {
+        sqlx::query!(
+            r#" 
+                INSERT
+                    INTO user_configs (
+                        user_id,
+                        allow_sharing_character_configs,
+                        allow_sharing_figure_records,
+                        updated_at,
+                        version
+                    ) VALUES ($1, $2, $3, $4, $5)
+                "#,
+            &user_config.user_id,
+            user_config.allow_sharing_character_configs,
+            user_config.allow_sharing_figure_records,
+            user_config.updated_at,
+            i32::try_from(user_config.version)?
+        )
+        .execute(&mut trx)
+        .await
+        .context("insert character_config")?;
+    } else {
+        let result = sqlx::query!(
+            r#"
+                UPDATE user_configs
+                    SET
+                        updated_at = $1,
+                        allow_sharing_character_configs = $2,
+                        allow_sharing_figure_records = $3,
+                        version = $4
+                    WHERE
+                        user_id = $5
+                        AND
+                        version = $6
+                "#,
+            &user_config
+                .updated_at
+                .ok_or(anyhow!("updated_at is None"))?,
+            user_config.allow_sharing_character_configs,
+            user_config.allow_sharing_figure_records,
+            i32::try_from(user_config.version)?,
+            &user_config.user_id,
+            i32::try_from(prev_version)?,
+        )
+        .execute(&mut trx)
+        .await
+        .context("update character_config")?;
 
-    if result.rows_affected() == 0 {
-        return Err(anyhow!("conflict"));
+        if result.rows_affected() == 0 {
+            return Err(anyhow!("conflict"));
+        }
     }
 
     trx.commit().await?;

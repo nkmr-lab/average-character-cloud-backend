bffba8633ae68d1e3662dfb5df16f353e01ad649
kgtkr <contact@kgtkr.net>
2022-12-31 01:30:13 +0900

    Figureをリファクタリング

--- src/entities/figure.rs
+++ src/entities/figure.rs
@@ -1,54 +1,56 @@
-use serde::{Deserialize, Serialize};
-
 use super::StrokeCount;
 
-/**
- * バックエンドで色々な処理することになったら別ライブラリとして分離したい
- */
+mod json_model {
+    use serde::{Deserialize, Serialize};
+    #[derive(Clone, Debug, Deserialize, Serialize)]
+    pub struct Figure {
+        pub strokes: Vec<Stroke>,
+        pub width: f64,
+        pub height: f64,
+    }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+    #[derive(Clone, Debug, Deserialize, Serialize)]
+    pub struct Stroke {
+        pub points: Vec<Point>,
+    }
+
+    #[derive(Clone, Debug, Deserialize, Serialize)]
+    pub struct Point {
+        pub x: f64,
+        pub y: f64,
+        pub z: f64,
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct Figure {
-    strokes: Vec<Stroke>,
-    width: f64,
-    height: f64,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Stroke {
-    points: Vec<Point>,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Point {
-    x: f64,
-    y: f64,
-    z: f64,
+    model: json_model::Figure,
+    stroke_count: StrokeCount,
 }
 
 impl Figure {
     pub fn to_json(&self) -> String {
-        serde_json::to_string(&self).unwrap()
+        serde_json::to_string(&self.model).unwrap()
     }
 
     pub fn to_json_ast(&self) -> serde_json::Value {
-        serde_json::to_value(self).unwrap()
+        serde_json::to_value(&self.model).unwrap()
     }
 
     pub fn from_json(json: &str) -> Option<Figure> {
         Self::from_json_ast(serde_json::from_str(json).ok()?)
     }
 
-    fn stroke_count_opt(&self) -> Option<StrokeCount> {
-        StrokeCount::try_from(i32::try_from(self.strokes.len()).ok()?).ok()
-    }
-
     pub fn from_json_ast(json: serde_json::Value) -> Option<Figure> {
-        let figure: Figure = serde_json::from_value(json).ok()?;
-        figure.stroke_count_opt()?;
-        Some(figure)
+        let model: json_model::Figure = serde_json::from_value(json).ok()?;
+        let stroke_count = StrokeCount::try_from(i32::try_from(model.strokes.len()).ok()?).ok()?;
+        Some(Figure {
+            model,
+            stroke_count,
+        })
     }
 
     pub fn stroke_count(&self) -> StrokeCount {
-        self.stroke_count_opt().unwrap()
+        self.stroke_count
     }
 }

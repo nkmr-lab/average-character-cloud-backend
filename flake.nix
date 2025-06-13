59ea5e6c08e37b367d22089ea294a45b93e12245
kgtkr <me@kgtkr.net>
2025-06-13 20:43:44 +0900

    rustcnix関連のアップデート

--- flake.nix
+++ flake.nix
@@ -3,51 +3,66 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    cargo2nix = {
+      url = "github:cargo2nix/cargo2nix/main";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
+    cargo2nix-ifd = {
+      url = "github:kgtkr/cargo2nix-ifd";
+      inputs.cargo2nix.follows = "cargo2nix";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, cargo2nix, cargo2nix-ifd, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [ cargo2nix.overlays.default ];
         pkgsArgs = {
           inherit system overlays;
         };
         pkgs = import nixpkgs pkgsArgs;
-        rust-toolchain = pkgs: pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        buildRustCrateForPkgs = pkgs: pkgs.buildRustCrate.override {
-          rustc = rust-toolchain pkgs;
-          defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-            "sqlx-macros" = attrs: {
-              buildInputs = with pkgs; lib.optionals stdenv.isDarwin [
-                darwin.apple_sdk.frameworks.SystemConfiguration
-                darwin.apple_sdk.frameworks.CoreFoundation
-              ];
-            };
-            "average-character-cloud-backend" = attrs: {
-              buildInputs = with pkgs; lib.optionals stdenv.isDarwin [
-                darwin.apple_sdk.frameworks.SystemConfiguration
-                darwin.apple_sdk.frameworks.Security
-              ];
-              SQLX_OFFLINE = "true";
-            };
-          };
+        cargo2nix-ifd-lib = cargo2nix-ifd.mkLib pkgs;
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        projectName = "average-character-cloud-backend";
+        src = ./.;
+        filteredSrc = cargo2nix-ifd-lib.filterSrc {
+          orFilter = path: _type:
+            let
+              files = [ "sqlx-data.json" ];
+            in
+            pkgs.lib.any (file: baseNameOf path == file) files;
+          inherit projectName src;
         };
-        crate = import ./Cargo.nix {
-          inherit pkgs buildRustCrateForPkgs;
+        generatedSrc = cargo2nix-ifd-lib.generateSrc {
+          src = filteredSrc;
+          inherit projectName rustToolchain;
+        };
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          packageFun = import "${generatedSrc}/Cargo.nix";
+          packageOverrides = pkgs: pkgs.rustBuilder.overrides.all ++
+            [
+              (pkgs.rustBuilder.rustLib.makeOverride {
+                name = "average-character-cloud-backend";
+                overrideAttrs = drv:
+                  {
+                    SQLX_OFFLINE = 1;
+                  };
+              })
+            ];
+          inherit rustToolchain;
         };
       in
-      with pkgs; rec {
-        defaultPackage = crate.rootCrate.build;
+      {
         packages = {
-          sqldef = callPackage ./sqldef.nix { };
-          average-character-cloud-backend = defaultPackage;
-          average-character-cloud-backend-docker = dockerTools.buildImage {
+          default = (self.packages.${system}.average-character-cloud-backend { }).bin;
+          average-character-cloud-backend = rustPkgs.workspace.average-character-cloud-backend;
+          average-character-cloud-backend-docker = pkgs.dockerTools.buildImage {
             name = "average-character-cloud-backend";
             contents = [
               pkgs.coreutils
               pkgs.bash
               pkgs.cacert
-              defaultPackage
+              self.packages.${system}.average-character-cloud-backend
             ];
             config = {
               Env = [ "AVCC_HOST=0.0.0.0" ];
@@ -55,26 +70,16 @@
             };
           };
         };
-        devShell = mkShell {
-          nativeBuildInputs = [
-            (rust-toolchain pkgs)
-            sqlx-cli
-            cargo-make
-            packages.sqldef
-            crate2nix
-            cargo-watch
-          ] ++ lib.optionals stdenv.isLinux [
-            pkg-config
-          ];
-
-          buildInputs = lib.optionals stdenv.isDarwin [
-            libiconv
-            darwin.apple_sdk.frameworks.SystemConfiguration
-            darwin.apple_sdk.frameworks.CoreFoundation
-            darwin.apple_sdk.frameworks.Security
-          ] ++ lib.optionals stdenv.isLinux [
-            openssl
-            glibc
+        devShell = pkgs.mkShell {
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+          packages = [
+            rustToolchain
+            pkgs.sqlx-cli
+            pkgs.cargo-make
+            pkgs.sqldef
+            pkgs.crate2nix
+            pkgs.cargo-watch
+            cargo2nix.packages.${system}.cargo2nix
           ];
         };
       }
